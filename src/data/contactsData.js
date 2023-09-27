ff52958075bb429a7d9021c58d991f57e4f5a0ed
sweetbanana-dev <corey.gray.dev@gmail.com>
2023-09-27 10:29:08 -0700

    main

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -3,5 +3,4 @@ export const contactsData = {
   phone: "+14057084300",
   address: "Los Angeles, California",
   github: "https://github.com/sweetbanana-dev",
-  linkedIn: "https://www.linkedin.com/in/henriequintanilla",
 };
